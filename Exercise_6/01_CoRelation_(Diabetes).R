151c1d4bc8b079f27d9554c0aa754f3d966ba9b5
varshithacherukuru <112241234+varshithacherukuru@users.noreply.github.com>
2023-01-11 21:44:33 -0800

    Create 01_CoRelation_(Diabetes).R

---
+++ Exercise_6/01_CoRelation_(Diabetes).R
@@ -0,0 +1,12 @@
+diabetes1<-read.csv("D:\\folders\\DWHDM\\diabetes.csv")
+
+#step 1
+diabetes1<-table(diabetes1 $Age,diabetes1 $Insulin)
+diabetes1
+
+#step 2
+chisq.test(diabetes1)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
